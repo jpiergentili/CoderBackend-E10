2b55cb0f871b99ec1a44c266663ec2630458c5c0
jpiergentili <javypier1@gmail.com>
2023-05-27 13:57:28 -0300

    Entrega10 - v3

--- src/app.js
+++ src/app.js
@@ -4,13 +4,28 @@ import handlebars from 'express-handlebars'
 import viewsRouter from './routes/views.router.js'
 import cartsRouter from './routes/carts.router.js'
 import cartRouter from './routes/cart.router.js'
-import cartModel from './models/cart.models.js'
+import sessionRouter from './routes/session.router.js'
+import session from 'express-session'
+import MongoStore from 'connect-mongo' 
+
 
 const uri = 'mongodb+srv://javypier1:Q1w2e3r4@jp-backend-coder01.bavi18s.mongodb.net/'
 
 const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+
+app.use(session({
+    store: MongoStore.create({
+        mongoUrl: uri,  //VER SI ESTA BIEN! 20230513 - 3:34:00
+        dbName: 'entrega10backend'
+    }),
+    secret: 'mysecret',
+    resave: true,
+    saveUninitialized: true
+}))
+app.use('/sessions', sessionRouter)
+
 
 //configuracion del motor de plantillas
 app.engine('handlebars', handlebars.engine())
@@ -24,11 +39,12 @@ app.use('/api/carts', cartsRouter);
 app.use('/products', viewsRouter);
 app.use('/carts', cartRouter);
 
+
 mongoose.set('strictQuery', false)
 
 try{
     await mongoose.connect(uri, {
-        dbName: 'entrega9backend'
+        dbName: 'entrega10backend'
     })
     console.log('DB connected!')
     
